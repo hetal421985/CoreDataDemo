bd288225bc05eddbed1bbb6f5862ee7a6cf7847f
hetal421985 <61933594+hetal421985@users.noreply.github.com>
2022-04-19 16:13:50 +0530

    Update export_ipa.sh
    Commit after modifying export .sh file

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -3,7 +3,6 @@
 set -eo pipefail
 
 xcodebuild -archivePath $PWD/build/Demo.xcarchive \
-            -exportOptionsPlist Demo/Demo/exportOptions.plist \
             -exportPath $PWD/build \
             -allowProvisioningUpdates \
             -exportArchive | xcpretty
