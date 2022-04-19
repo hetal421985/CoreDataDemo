202345fc19704f84364b5bced9b98e7a377275d7
hetal421985 <61933594+hetal421985@users.noreply.github.com>
2022-04-19 17:37:17 +0530

    Update export_ipa.sh

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 
 xcodebuild  -archivePath $PWD/build/Demo.xcarchive \
             -exportOptionsPlist exportOptions.plist \
-            -exportPath DerivedData/ipa \
+             -exportPath "$XC_EXPORT_PATH"
             -allowProvisioningUpdates \
+            -exportArchive -archivePath "$XC_ARCHIVE_PATH"
            
